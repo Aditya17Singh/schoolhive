1549577be1171159ddf473b4ffd2eb418a0e24eb
Aditya Singh <adiyasngh14@gmail.com>
2025-10-14 10:59:46 +0530

    env added

--- Frontend-web/lib/api.js
+++ Frontend-web/lib/api.js
@@ -1,14 +1,34 @@
+// // lib/api.js
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5001/api",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// // Inject token into every request
+// API.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default API;
+
 // lib/api.js
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// Inject token into every request
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
